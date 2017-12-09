89f05b26c80d3c1768a75e94852115e9a716df4a
AT-Tool <34391378+AT-Tool@users.noreply.github.com>
2017-12-09 19:54:18 +0700

    Update User.lua

--- User.lua
+++ User.lua
@@ -1,4 +1,4 @@
 purplepanda588|DF9dQBQLDAULRhcTEg==|DF9dQBQLDAULRhoTEg==
 smallsnake370|DF9dQBQLDAULRhcTEg==|DF9dQBQLDAULRhoTEg==
-Sever3|Pass3|PassAo3
-Sever4|Pass4|PassAo4
+purplepanda588|DF9dQBQLDAULRhcTEg==|DF9dQBQLDAULRhoTEg==
+smallsnake370|DF9dQBQLDAULRhcTEg==|DF9dQBQLDAULRhoTEg==
